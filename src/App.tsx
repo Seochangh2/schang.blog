6192ca7fc2ac39e998022f98b799c8a7fdc420f5
Seochangh2 <nomm89@naver.com>
2024-07-04 22:03:15 +0900

    대문 구성요소 및 레이아웃

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-
+import styled from "styled-components";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+import PostsContainer from "./components/PostsContainer";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
+    <Container>
+      <Header></Header>
+      <Contents>
+        <Sidebar></Sidebar>
+        <PostsContainer></PostsContainer>
+      </Contents>
+    </Container>
   );
 }
 
 export default App;
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+  overflow-y: auto;
+`;
+const Contents = styled.div`
+  display: flex;
+  justify-content: start;
+  align-items: center;
+  width: 1250px;
+`;
---
+++ src/components/SearchBox.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+const SearchBox = () => {
+  return <Container></Container>;
+};
+
+export default SearchBox;
+const Container = styled.div`
+  margin-top: 20%;
+  height: 80%;
+  width: 20%;
+  background-color: #4a4a4a23;
+  margin-right: 5%;
+  border-radius: 10px;
+`;
